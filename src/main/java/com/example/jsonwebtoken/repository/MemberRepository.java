64738f9e4cef2a0cc8a851a5b316cce1ed8e88fa
Kim-SiHwan <tjdans4166@naver.com>
2021-01-22 21:15:39 +0900

    멤버Repo

---
+++ src/main/java/com/example/jsonwebtoken/repository/MemberRepository.java
@@ -0,0 +1,10 @@
+package com.example.jsonwebtoken.repository;
+
+import com.example.jsonwebtoken.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<Member,Long> {
+    Optional<Member> findByUsername(String username);
+}
