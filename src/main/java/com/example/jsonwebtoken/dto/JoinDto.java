77fd4c25605d241798507fda8fa1580fd0e9e45e
Kim-SiHwan <tjdans4166@naver.com>
2021-01-22 21:14:18 +0900

    회원가입Dto

---
+++ src/main/java/com/example/jsonwebtoken/dto/JoinDto.java
@@ -0,0 +1,23 @@
+package com.example.jsonwebtoken.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class JoinDto {
+    @NotNull
+    private String username;
+
+    @NotNull
+    private String password;
+
+}
